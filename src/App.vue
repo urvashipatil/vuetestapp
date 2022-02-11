bac588f71c807f013544e2a6aa3acc681af9b72c
Urvashi Amit Sachdev <urvashi.sachdev@algorisys.com>
2022-02-11 10:01:22 +0530

    commited code of Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+    <router-link to="/nestedapp">Nested App</router-link> | 
+    <router-link to="/hello">Hello World</router-link> | 
+      <router-link to="/checkbox">Checkbox</router-link> | 
+    <!-- <router-link to="/checkbox/100">Checkbox</router-link> |  -->
+
+    <router-view />    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App',    
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.router-link-active{
+  background: red;
+  color: white;
 }
 </style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,72 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import NestedApp from "./components/nesting-components/NestedApp.vue";
+// import FormDynamic from "./components/slots/FormDynamic.vue";
+import CheckboxSelect from "./components/checkbox-select.vue";
+import { store } from "./store/store";
+// import Projects from "./components/Projects.vue";
 
-Vue.config.productionTip = false
+// Vue.component("projects", Projects);
+
+Vue.use(VueRouter);
+
+//Create new instance if VueRouter
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/hello", component: HelloWorld },
+    { path: "/nestedapp", component: NestedApp },
+    { path: "/checkbox", component: CheckboxSelect },
+    { path: "/checkbox/:id", component: CheckboxSelect },
+    // {
+    //   path: "/checkbox",
+    //   component: CheckboxSelect,
+    //   children: [
+    //     {
+    //       path: "nestedapp",
+    //       component: NestedApp,
+    //     },
+    //   ],
+    // },
+  ],
+});
+
+Vue.directive("random", {
+  bind(el, binding, vNode) {
+    console.log("binding", binding);
+    console.log("vNode", vNode);
+    el.style.color = "#" + Math.random().toString().slice(2, 8);
+    el.style.fontWeight = "bold";
+  },
+});
+
+// Vue.directive("theme", {
+//   bind(el, binding) {
+//     if (binding.value == "wide") {
+//       el.style.background = "lightgray";
+//       el.style.width = "1200px";
+//     } else if (binding.value == "narrow") {
+//       el.style.background = "cyan";
+//       el.style.width = "600px";
+//     }
+
+//     if (binding.arg == "column") {
+//       el.style.padding = "20px";
+//       el.style.background = "yellow";
+//     }
+//   },
+// });
+
+// Vue.filter("mycase", function (data) {
+//   return (data || "").toUpperCase();
+// });
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  render: (h) => h(App),
+  router: router,
+}).$mount("#app");
